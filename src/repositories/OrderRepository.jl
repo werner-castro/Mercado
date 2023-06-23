715ec9f25ac2dfd109eb430375d7e8725528a7c1
werner <carlos.werner@pathfind.com.br>
2023-06-23 12:07:28 -0300

    na query do order repository

--- src/repositories/OrderRepository.jl
+++ src/repositories/OrderRepository.jl
@@ -9,8 +9,16 @@ export select_order_by_date_and_total
 
 # função para selecionar pedido por data e total
 function select_order_by_date_and_total(date::String, totals::Float64)
-    query = From(:order) |> Where( Fun.and( Get.date .== date, Get.totals .== totals ) ) |> Select(Get.id, Get.client, Get.order_date, Get.totals)
-    find(Order, SQLWhereExpression(query))
+    query = From(:order)|> 
+        Where( 
+            Fun.and( 
+                Get.date .== date, 
+                Get.totals .== totals 
+            ) 
+        )|> 
+        Select(Get.id, Get.client, Get.order_date, Get.totals)
+    
+    return find(Order, SQLWhereExpression(query))
 end
 
 end # module
--- src/repositories/UserRepository.jl
+++ src/repositories/UserRepository.jl
@@ -8,6 +8,7 @@ export select_by_email
 function select_by_email(email::String)
     user = User(email=email)
     result = find(User, email = user.email)
+    
     return result
 end
 
